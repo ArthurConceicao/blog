ba850df369e113cc604a33d71d24fa195233ef72
arthur.conceicao <arthur.conceicao@crptecnologia.com.br>
2018-07-18 11:41:44 -0300

    Criação dos componentes top e panel

---
+++ resources/assets/js/components/Panel.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="setColor">
+        <div class="panel-heading">{{ title }}</div>
+
+        <div class="panel-body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Panel",
+        props: ['title','color'],
+        computed: {
+            setColor: function () {
+                return "panel " + (this.color || "panel-default");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-  <example></example>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <panel title="Painel">
+                    Conteúdo do Painel
+                    <div class="row">
+                        <div class="col-md-4">
+                            <panel title="Conteúdo 1" color="panel-success">
+                                Texto do conteúdo 1
+                            </panel>
+                        </div>
+                        <div class="col-md-4">
+                            <panel title="Conteúdo 2">
+                                Texto do conteúdo 2
+                            </panel>
+                        </div>
+                        <div class="col-md-4">
+                            <panel title="Conteúdo 3">
+                                Texto do conteúdo 4
+                            </panel>
+                        </div>
+                    </div>
+                </panel>
+            </div>
+        </div>
+    </div>
 @endsection
